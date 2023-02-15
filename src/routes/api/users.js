d43c97596c96974ef5b2abfda8b450af50446cb1
BuildTools <unconfigured@null.spigotmc.org>
2023-02-15 14:03:16 -0600

    finalised user registering

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const gravatar = require("gravatar")
 const { check, validationResult } = require("express-validator")
 const bcrypt = require("bcryptjs")
+const config = require("config")
+const jwt = require("jsonwebtoken")
 
 const User = require("../../../models/User")
 
@@ -11,57 +13,71 @@ const User = require("../../../models/User")
 // @desc   : register user
 // @access : public
 router.post(
-    "/",
-    [
-        check("name", "Name is required").not().isEmpty(),
-        check("email", "Please include a vaild email").isEmail(),
-        check(
-            "password",
-            "Please enter a password with 2 or more characters",
-        ).isLength({ min: 2 }),
-    ],
-    async (req, res) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
-        }
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a vaild email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 2 or more characters",
+    ).isLength({ min: 2 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
 
-        const { name, email, password } = req.body
+    const { name, email, password } = req.body
 
-        try {
-            let user = await User.findOne({ email })
+    try {
+      let user = await User.findOne({ email })
 
-            if (user) {
-                return res.status(400).json({
-                    error: [{ msg: "User already exists" }],
-                })
-            }
+      if (user) {
+        return res.status(400).json({
+          error: [{ msg: "User already exists" }],
+        })
+      }
 
-            const avatar = gravatar.url(email, {
-                s: "200",
-                r: "X",
-                d: "mm",
-            })
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "X",
+        d: "mm",
+      })
 
-            user = new User({
-                name,
-                email,
-                avatar,
-                password,
-            })
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      })
 
-            const salt = await bcrypt.genSalt(10)
+      const salt = await bcrypt.genSalt(10)
 
-            user.password = await bcrypt.hash(password, salt)
+      user.password = await bcrypt.hash(password, salt)
 
-            await user.save()
+      await user.save()
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      }
 
-            //return webtoken
-            res.send("user roegistered")
-        } catch (err) {
-            console.error(err.message)
-            res.status(500).send("server error")
-        }
-    },
+      //TODO change expire time to 3600 (1 hour) before going into production
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err
+          res.json({ token })
+        },
+      )
+    } catch (err) {
+      console.error(err.message)
+      res.status(500).send("server error")
+    }
+  },
 )
 module.exports = router
