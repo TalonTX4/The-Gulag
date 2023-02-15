3eb0c369e7128e2dadb0367618feeb98aa71c062
BuildTools <unconfigured@null.spigotmc.org>
2023-02-15 11:02:19 -0600

    created basic express server and tested get with postman

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 // imports
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
 
-app.get("/", (req, res) => res.send("API running"))
+app.get("/", (req, res) => res.send("API running"));
 
 // use environment port or 5000
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 //NOTE ${varName} adds in the varName variable to the string (only works with `` (tilde) not "" (quotes))
-app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
