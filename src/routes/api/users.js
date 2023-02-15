dfe959aeaff24791900862b5dc3be47ad2d51380
BuildTools <unconfigured@null.spigotmc.org>
2023-02-15 13:27:10 -0600

    implemented checks for password length, email being plausible, and name existing

--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -1,11 +1,27 @@
 //imports
 const express = require("express")
-
 const router = express.Router()
+const { check, validationResult } = require("express-validator/check")
 
-// @route  : GET api/users
-// @desc   : Test route
+// @route  : POST api/users
+// @desc   : register user
 // @access : public
-router.get("/", (req, res) => res.send("user route"))
-
+router.post(
+    "/",
+    [
+        check("name", "Name is required").not().isEmpty(),
+        check("email", "Please include a vaild email").isEmail(),
+        check(
+            "password",
+            "Please enter a password with 2 or more characters",
+        ).isLength({ min: 2 }),
+    ],
+    (req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        res.send("user route")
+    },
+)
 module.exports = router
--- src/server.js
+++ src/server.js
@@ -7,6 +7,9 @@ const app = express()
 // Connect Database
 connectDB()
 
+// init middleware
+app.use(express.json({ extended: false }))
+
 app.get("/", (req, res) => res.send("API running"))
 
 // define routes
