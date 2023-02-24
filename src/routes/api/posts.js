aae1a3249ac32b96543f6e779c8c4716f2d5cc6d
BuildTools <unconfigured@null.spigotmc.org>
2023-02-24 11:59:34 -0600

    dryed posts by moving to sub folders

--- src/routes/api/posts.js
+++ src/routes/api/posts.js
@@ -7,6 +7,11 @@ const User = require("../../../models/User")
 const Post = require("../../../models/Post")
 const errorHandler = require("../../misc/errors")
 
+// sub-routes
+router.use("/comment", require("./posts/comment"))
+router.use("/like", require("./posts/like"))
+router.use("/unlike", require("./posts/unlike"))
+
 // @route  : POST api/posts
 // @desc   : Create a post
 // @access : Private
@@ -81,124 +86,6 @@ router.delete("/:id", jwtVerify, async (req, res) => {
     res.json({ msg: "post removed" })
   } catch (err) {
     errorHandler.serverObjectId(res, err, "Post")
-  }
-})
-
-// @route  : PUT api/posts/like/:id
-// @desc   : Like a post
-// @access : Private
-router.put("/like/:id", jwtVerify, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id)
-
-    if (
-      post.likes.filter((like) => like.user.toString() === req.user.id).length >
-      0
-    )
-      return errorHandler.generic(res, "postLike")
-
-    post.likes.unshift({ user: req.user.id })
-
-    await post.save()
-
-    res.json(post.likes)
-  } catch (err) {
-    errorHandler.serverError(res, err)
-  }
-})
-
-// @route  : PUT api/posts/unlike/:id
-// @desc   : Unlike a post
-// @access : Private
-router.put("/unlike/:id", jwtVerify, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id)
-
-    if (
-      post.likes.filter((like) => like.user.toString() === req.user.id)
-        .length === 0
-    )
-      return errorHandler.generic(res, "postUnlike")
-
-    // Get remove index
-    const removeIndex = post.likes
-      .map((like) => like.user.toString())
-      .indexOf(req.user.id)
-
-    post.likes.splice(removeIndex, 1)
-
-    await post.save()
-
-    res.json(post.likes)
-  } catch (err) {
-    errorHandler.serverError(res, err)
-  }
-})
-
-// @route  : POST api/posts/comment/:id
-// @desc   : Comment on a post
-// @access : Private
-router.post(
-  "/comment/:id",
-  [jwtVerify, [check("text", "text is required").not().isEmpty()]],
-  async (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) return errorHandler.validatorReturn(res, errors)
-
-    try {
-      const user = await User.findById(req.user.id).select("-password")
-      const post = await Post.findById(req.params.id)
-
-      const newComment = {
-        text: req.body.text,
-        name: user.name,
-        avatar: user.avatar,
-        user: req.user.id,
-      }
-
-      post.comments.unshift(newComment)
-
-      await post.save()
-
-      res.json(post.comments)
-    } catch (err) {
-      errorHandler.serverError(res, err)
-    }
-  }
-)
-
-// @route  : DELETE api/posts/comments/:id/:comment_id
-// @desc   : Delete a comment
-// @access : Private
-router.delete("/comments/:id/:comment_id", jwtVerify, async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id)
-
-    //pull out comment
-    const comment = post.comments.find(
-      (comment) => comment.id === req.params.comment_id
-    )
-
-    //check if comment exists
-
-    if (!comment) return errorHandler.notFound(res, "Comment")
-
-    // Check user
-    if (comment.user.toString() !== req.user.id)
-      return errorHandler.authError(res)
-
-    // Get remove index
-    const removeIndex = post.comments
-      .map((comment) => comment.user.toString())
-      .indexOf(req.user.id)
-
-    post.comments.splice(removeIndex, 1)
-
-    await post.save()
-
-    res.json(post.comments)
-  } catch (err) {
-    errorHandler.serverObjectId(res, err, "Comment")
   }
 })
 
