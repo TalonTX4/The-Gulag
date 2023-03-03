195768b510b92aa72d46931febc3717cf23a6563
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 14:25:35 -0600

    implemented alerts to register

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
+import Alert from "./components/layout/Alert"
 // Redux
 import { Provider } from "react-redux"
 import store from "./store"
@@ -14,11 +15,14 @@ const App = () => (
   <Provider store={store}>
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      <section className="container">
+        <Alert />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </section>
     </Router>
   </Provider>
 )
