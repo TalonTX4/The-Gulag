138b2a5eadb41f5d38eda553d24cd79de4d37a2e
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 13:22:54 -0600

    implemented redux store

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,23 @@ import Navbar from "./components/layout/Navbar"
 import Landing from "./components/layout/Landing"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
+// Redux
+import { Provider } from "react-redux"
+import store from "./store"
+
 import "./App.css"
 
 const App = () => (
-  <Router>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Landing />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+  </Provider>
 )
 
 export default App
