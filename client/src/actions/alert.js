1d8897c1874f73deb62acd040d0ae846021df903
BuildTools <unconfigured@null.spigotmc.org>
2023-03-03 14:03:51 -0600

    fixed previous fix to fix uuid

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuidV4 } from "uuid"
 import { SET_ALERT, REMOVE_ALERT } from "./types"
 
 export const setAlert = (msg, alertType) => (dispatch) => {
-  const id = uuidV4
+  const id = uuidV4()
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id },
