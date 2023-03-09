9edb27b924557aae4b90b2e920b70143f1a0cf1c
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 12:56:57 -0600

    improved css

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Landing from "./components/layout/Landing"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
 import Alert from "./components/layout/Alert"
+import Dashboard from "./components/dashboard/dashboard"
 // Redux
 import { Provider } from "react-redux"
 import store from "./store"
@@ -26,14 +27,13 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Navbar />
-        <section className="container">
-          <Alert />
-          <Routes>
-            <Route path="/" element={<Landing />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </section>
+        <Alert />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
       </Router>
     </Provider>
   )
