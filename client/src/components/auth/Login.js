08fb969ec2ef7d2846a420c6d36d01c16629d896
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 09:38:37 -0600

    implemented login backend connector

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { Fragment, useState } from "react"
 import { Link } from "react-router-dom"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import { login } from "../../actions/auth"
 
-const Login = () => {
+const Login = ({ login }) => {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -12,9 +15,9 @@ const Login = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value })
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
-    console.log("success")
+    login(email, password)
   }
 
   return (
@@ -54,4 +57,8 @@ const Login = () => {
   )
 }
 
-export default Login
+login.propTypes = {
+  login: PropTypes.func.isRequired,
+}
+
+export default connect(null, { login })(Login)
