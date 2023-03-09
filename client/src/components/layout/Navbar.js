0a6c22eb8553aa7f519b4c593436b3e3755e6a02
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 16:18:24 -0600

    replacing dev connector branding with placeholders

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const guestLinks = (
     <ul>
       <li>
-        <Link to="/profiles">Developers</Link>
+        <Link to="/profiles">!!!USER-PLURALITY</Link>
       </li>
       <li>
         <Link to="/register">Register</Link>
@@ -40,7 +40,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
     <nav className="navbar bg-dark">
       <h1>
         <Link to="/">
-          <i className="fas fa-code"></i> DevConnector
+          <i className="">!!!FA-ICON-IN-CLASS</i> !!!WEBSITE-NAME-SHORT
         </Link>
       </h1>
       <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
