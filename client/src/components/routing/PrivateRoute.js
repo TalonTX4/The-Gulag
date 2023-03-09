2d7a7a588af7244970edf9ab32d45d84c3112dbb
BuildTools <unconfigured@null.spigotmc.org>
2023-03-09 17:32:07 -0600

    clear profile when logout

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Navigate } from "react-router-dom"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
-import Spinner from "../layout/Spinner"
+import Spinner from "../layout/spinner"
 
 const PrivateRoute = ({
   component: Component,
