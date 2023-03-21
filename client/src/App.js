e78819aebab4e137a8fd5ec198c97e961c89d66d
BuildTools <unconfigured@null.spigotmc.org>
2023-03-21 13:25:30 -0500

    moved localStorage.token into use effect & added all routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,18 @@ import Landing from "./components/layout/Landing"
 import Login from "./components/auth/Login"
 import Register from "./components/auth/Register"
 import Alert from "./components/layout/Alert"
-import Dashboard from "./components/dashboard/dashboard"
-import CreateProfile from "./components/profile-form/CreateProfile"
+import Dashboard from "./components/dashboard/Dashboard"
+import ProfileForm from "./components/profile-forms/ProfileForm"
+import AddExperience from "./components/profile-forms/AddExperience"
+import AddEducation from "./components/profile-forms/AddEducation"
+import Profiles from "./components/profiles/Profiles"
+import Profile from "./components/profile/Profile"
+import Posts from "./components/posts/Posts"
+import Post from "./components/post/Post"
+import NotFound from "./components/layout/NotFound"
 import PrivateRoute from "./components/routing/PrivateRoute"
+import { LOGOUT } from "./actions/types"
+
 // Redux
 import { Provider } from "react-redux"
 import store from "./store"
@@ -16,13 +25,21 @@ import setAuthToken from "./utils/setAuthToken"
 
 import "./App.css"
 
-if (localStorage.token) {
-  setAuthToken(localStorage.token)
-}
-
 const App = () => {
   useEffect(() => {
-    store.dispatch(loadUser)
+    // check for token in LS when app first runs
+    if (localStorage.token) {
+      // if there is a token set axios headers for all requests
+      setAuthToken(localStorage.token)
+    }
+    // try to fetch a user, if no token or invalid token we
+    // will get a 401 response from our API
+    store.dispatch(loadUser())
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener("storage", () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT })
+    })
   }, [])
 
   return (
@@ -34,14 +51,34 @@ const App = () => {
           <Route path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/profiles" element={<Profiles />} />
+          <Route path="/profile/:id" element={<Profile />} />
           <Route
             path="/dashboard"
             element={<PrivateRoute component={Dashboard} />}
           />
           <Route
             path="/create-profile"
-            element={<PrivateRoute component={CreateProfile} />}
+            element={<PrivateRoute component={ProfileForm} />}
           />
+          <Route
+            path="/edit-profile"
+            element={<PrivateRoute component={ProfileForm} />}
+          />
+          <Route
+            path="/add-experience"
+            element={<PrivateRoute component={AddExperience} />}
+          />
+          <Route
+            path="/add-education"
+            element={<PrivateRoute component={AddEducation} />}
+          />
+          <Route path="/posts" element={<PrivateRoute component={Posts} />} />
+          <Route
+            path="/posts/:id"
+            element={<PrivateRoute component={Post} />}
+          />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </Router>
     </Provider>
