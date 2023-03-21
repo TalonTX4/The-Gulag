17277cdc79568421bc0b2b77f65ed020d2b803d8
BuildTools <unconfigured@null.spigotmc.org>
2023-03-21 13:31:20 -0500

    changed auth to work with api

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,4 @@
-import axios from "axios"
+import api from "../utils/api"
 import { setAlert } from "./alert"
 import {
   REGISTER_SUCCESS,
@@ -8,18 +8,19 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
-  CLEAR_PROFILE,
 } from "./types"
-import setAuthToken from "../utils/setAuthToken"
 
-// Load user
+/*
+  NOTE: we don't need a config object for axios as the
+ default headers in axios are already Content-Type: application/json
+ also axios stringifies and parses JSON for you, so no need for
+ JSON.stringify or JSON.parse
+*/
+
+// Load User
 export const loadUser = () => async (dispatch) => {
-  if (localStorage.token) {
-    setAuthToken(localStorage.token)
-  }
-
   try {
-    const res = await axios.get("/api/auth")
+    const res = await api.get("/auth")
 
     dispatch({
       type: USER_LOADED,
@@ -32,53 +33,35 @@ export const loadUser = () => async (dispatch) => {
   }
 }
 
-// Register user
-export const register =
-  ({ name, email, password }) =>
-  async (dispatch) => {
-    const config = {
-      headers: {
-        "Content-Type": "application/json",
-      },
+// Register User
+export const register = (formData) => async (dispatch) => {
+  try {
+    const res = await api.post("/users", formData)
+
+    dispatch({
+      type: REGISTER_SUCCESS,
+      payload: res.data,
+    })
+    dispatch(loadUser())
+  } catch (err) {
+    const errors = err.response.data.errors
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, "danger")))
     }
 
-    const body = JSON.stringify({ name, email, password })
-
-    try {
-      const res = await axios.post("/api/users", body, config)
-      console.log(res)
-
-      dispatch({
-        type: REGISTER_SUCCESS,
-        payload: res.data,
-      })
-
-      dispatch(loadUser())
-    } catch (err) {
-      const errors = err.response.data.error
-
-      if (errors) {
-        errors.forEach((error) => dispatch(setAlert(error.msg, "danger")))
-      }
-
-      dispatch({
-        type: REGISTER_FAIL,
-      })
-    }
+    dispatch({
+      type: REGISTER_FAIL,
+    })
   }
+}
 
-// Login user
+// Login User
 export const login = (email, password) => async (dispatch) => {
-  const config = {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }
-
-  const body = JSON.stringify({ email, password })
+  const body = { email, password }
 
   try {
-    const res = await axios.post("/api/auth", body, config)
+    const res = await api.post("/auth", body)
 
     dispatch({
       type: LOGIN_SUCCESS,
@@ -87,7 +70,7 @@ export const login = (email, password) => async (dispatch) => {
 
     dispatch(loadUser())
   } catch (err) {
-    const errors = err.response.data.error
+    const errors = err.response.data.errors
 
     if (errors) {
       errors.forEach((error) => dispatch(setAlert(error.msg, "danger")))
@@ -99,8 +82,5 @@ export const login = (email, password) => async (dispatch) => {
   }
 }
 
-// Logout / clear profile
-export const logout = () => (dispatch) => {
-  dispatch({ type: CLEAR_PROFILE })
-  dispatch({ type: LOGOUT })
-}
+// Logout
+export const logout = () => ({ type: LOGOUT })
