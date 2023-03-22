dd8c0a15c388c955d6e91ce294924215de02064a
BuildTools <unconfigured@null.spigotmc.org>
2023-03-22 14:39:12 -0500

    preparing for heroku deployment

--- src/server.js
+++ src/server.js
@@ -10,8 +10,6 @@ connectDB().then()
 // init middleware
 app.use(express.json({ extended: false }))
 
-app.get("/", (req, res) => res.send("API running"))
-
 // define routes
 //NOTE this is the good way to pass app to api lol
 app.use("/api/users", require("./routes/api/users"))
@@ -19,8 +17,18 @@ app.use("/api/auth", require("./routes/api/auth"))
 app.use("/api/profile", require("./routes/api/profile"))
 app.use("/api/posts", require("./routes/api/posts"))
 
-// use port set in config
-const PORT = config.get("backendConnector.serverPort")
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("../client/build"))
+
+  app.get("*", (req, res) => {
+    res.sendFile("../client/build/index.html")
+  })
+}
+
+// use port in env or port set in config
+const PORT = process.env.PORT || config.get("backendConnector.serverPort")
 
 //NOTE ${varName} adds in the varName variable to the string (only works with `` (tilde) not "" (quotes))
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
