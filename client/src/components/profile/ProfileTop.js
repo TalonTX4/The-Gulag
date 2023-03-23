34800d24c3cf0e01f9310b78c3d4edcd44b3a7d7
BuildTools <unconfigured@null.spigotmc.org>
2023-03-22 22:28:42 -0500

    removed website due to bugs

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react"
+import PropTypes from "prop-types"
 
 const ProfileTop = ({
   profile: {
@@ -8,8 +8,8 @@ const ProfileTop = ({
     location,
     website,
     social,
-    user: { name, avatar }
-  }
+    user: { name, avatar },
+  },
 }) => {
   return (
     <div className="profile-top bg-primary p-2">
@@ -19,33 +19,37 @@ const ProfileTop = ({
         {status} {company ? <span> at {company}</span> : null}
       </p>
       <p>{location ? <span>{location}</span> : null}</p>
+      {/*
+// TODO fix website links not redirecting to external site
+// TODO re-add website as a field in profile
+
       <div className="icons my-1">
         {website ? (
           <a href={website} target="_blank" rel="noopener noreferrer">
             <i className="fas fa-globe fa-2x" />
           </a>
-        ) : null}
-        {social
-          ? Object.entries(social)
-              .filter(([_, value]) => value)
-              .map(([key, value]) => (
-                <a
-                  key={key}
-                  href={value}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <i className={`fab fa-${key} fa-2x`}></i>
-                </a>
-              ))
-          : null}
-      </div>
+        ) : null}*/}
+      {social
+        ? Object.entries(social)
+            .filter(([_, value]) => value)
+            .map(([key, value]) => (
+              <a
+                key={key}
+                href={value}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className={`fab fa-${key} fa-2x`}></i>
+              </a>
+            ))
+        : null}
+      {/*      </div>*/}
     </div>
-  );
-};
+  )
+}
 
 ProfileTop.propTypes = {
-  profile: PropTypes.object.isRequired
-};
+  profile: PropTypes.object.isRequired,
+}
 
-export default ProfileTop;
+export default ProfileTop
