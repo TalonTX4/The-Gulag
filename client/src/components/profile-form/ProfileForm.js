91241c643533995f7ebb3aa5cf23185b532e94b3
BuildTools <unconfigured@null.spigotmc.org>
2023-03-22 22:34:56 -0500

    added comment to prevent future people from making my mistakes

--- client/src/components/profile-form/ProfileForm.js
+++ client/src/components/profile-form/ProfileForm.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux"
 import { createProfile, getCurrentProfile } from "../../actions/profile"
 import { formConstructor } from "../../constructors/formConstructor"
 
+// declaring initialState outside of profileForm to preemptively set all values to null
 const initialState = {
   company: "",
   website: "",
