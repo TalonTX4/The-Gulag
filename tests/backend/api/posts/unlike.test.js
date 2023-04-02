afdcef41c7a5a9615b611e747a709f75d9530ace
BuildTools <unconfigured@null.spigotmc.org>
2023-04-02 12:07:19 -0500

    filled unlike routes

---
+++ tests/backend/api/posts/unlike.test.js
@@ -0,0 +1,69 @@
+const request = require("supertest")
+const app = require("../../../../app")
+const dbConnector = require("../../../db-connector")
+
+let mockId
+let mockPostId
+
+jest.mock("../../../../middleware/jwtVerify", () => async (req, res, next) => {
+  req.user = { id: await mockId }
+  return next()
+})
+
+let server
+
+beforeAll(async () => {
+  server = app.listen(0, (err) => {
+    if (err) return err
+  })
+  await dbConnector.connect()
+})
+
+afterAll(async () => {
+  await server.close()
+  await dbConnector.closeDatabase()
+})
+
+beforeEach(async () => {
+  await dbConnector.fillDatabase()
+  mockId = await dbConnector.testUser()
+  mockPostId = await dbConnector.testPost()
+  await dbConnector.testLike()
+})
+
+afterEach(async () => {
+  jest.restoreAllMocks()
+  await dbConnector.clearDatabase()
+})
+
+test("Server start", () => {})
+
+describe("PUT api/posts/unlike/:id", () => {
+  test("liking case", (done) => {
+    request(server)
+      .put(`/api/posts/unlike/${mockPostId}`)
+      .send({})
+      .expect(200)
+      .end((err) => {
+        if (err) return done(err)
+        return done()
+      })
+  })
+
+  test("no like case", (done) => {
+    request(server)
+      .put(`/api/posts/unlike/${mockPostId}`)
+      .send({})
+      .expect(200)
+      .end(() => {
+        request(server)
+          .put(`/api/posts/unlike/${mockPostId}`)
+          .send({})
+          .expect(400)
+          .end((err) => {
+            if (err) return done(err)
+            return done()
+          })
+      })
+  })
+})
